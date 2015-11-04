91ae879afe77978f0ade00a6bde32bc6fafde29f
Jason McVetta <jason.mcvetta@gmail.com>
2015-11-04 02:45:29 -0800

    test service is running and enabled, /opt/howsmyssl folder exists

--- test/integration/default/serverspec/howsmyssl_spec.rb
+++ test/integration/default/serverspec/howsmyssl_spec.rb
@@ -9,3 +9,12 @@ end
 describe port(443) do
 	it { should be_listening }
 end
+
+describe service('howsmyssl') do
+	it { should be_running }
+	it { should be_enabled }
+end
+
+describe file('/opt/howsmyssl') do
+	it { should be_directory }
+end
